c3457585f6f09c71e62dd589465626d1353c442b
277heena <277heena@gmail.com>
2026-05-04 21:03:47 -0700

    Update Node.js examples and add node-http project

--- node-example2.js
+++ node-example2.js
@@ -1,4 +1,3 @@
-const { PI } = Math;  // private to this file
+exports.area = (r) => Math.PI * r * r;
 
-area = (r) => PI * r ** 2;  // exported
-circumference = (r) => 2 * PI * r;  // exported
+exports.circumference = (r) => 2 * Math.PI * r;
